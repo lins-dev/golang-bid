dcbb75736d273c7dd7b64720c4ee6a4ec726b01a
Lucas Lins <lucas.fsl.92@gmail.com>
2025-03-12 23:21:17 -0300

    feature: adding SQLc and tern configuration

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,16 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("Hello World!")
+	fmt.Println("Hello World!!!!!")
 	fmt.Println("API")
 }
+
+// run Air
+// air --build.cmd "go build -o ./bin/api ./cmd/api" --build.bin "./bin/api"
+
+// create migrations
+// in folder: internal/store/pgstore/migrations
+// tern new create_users_table
+
+// generate SQLc files
+// sqlc generate -f ./internal/store/pgstore/sqlc.yml
---
+++ internal/store/pgstore/queries/users.sql
@@ -0,0 +1,34 @@
+-- name: CreateUser :one
+INSERT INTO users("user_name", "email", "password_hash", "bio")
+VALUES($1, $2, $3, $4)
+    RETURNING *;
+
+-- name: GetUser :one
+SELECT * FROM users
+WHERE id = $1 LIMIT 1;
+
+-- name: GetUserByUuid :one
+SELECT * FROM users
+WHERE uuid = $1 LIMIT 1;
+
+-- name: ListUsers :many
+SELECT * FROM users
+ORDER BY email;
+
+-- name: UpdateUser :one
+UPDATE users
+    set user_name = $2,
+    email = $3,
+    password_hash = $4,
+    bio = $5
+WHERE id = $1
+RETURNING *;
+
+-- name: UpdateUserByUuid :one
+UPDATE users
+    set user_name = $2,
+    email = $3,
+    password_hash = $4,
+    bio = $5
+WHERE uuid = $1
+RETURNING *;
